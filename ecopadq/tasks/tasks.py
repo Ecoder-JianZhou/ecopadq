db207a6e070e092f9378fe0c350a030d8225b6bf
Ecoder-JianZhou <ecoder-zj@outlook.com>
2022-10-25 00:24:33 -0400

    update 2022-10-24-8

--- ecopadq/tasks/tasks.py
+++ ecopadq/tasks/tasks.py
@@ -106,21 +106,21 @@ def run_forecast(self, model_name, site_name): #def teco_spruce_forecast(pars,fo
         params["Rs0"] = paraset_array[i,17]
         params["Rr0"] = paraset_array[i,18] 
         # create param file 
-        os.makedirs(resultDir+"/input_"+str(i))
-        os.makedirs(resultDir+"/output_"+str(i))
-        param_filename = create_template(input_files['tmpl_pars'], 'pars',params,resultDir+"/input_"+str(i),check_params, input_files['def_ls_pars'])
+        os.makedirs(resultDir+"/input/input_"+str(i))
+        os.makedirs(resultDir+"/output/output_"+str(i))
+        param_filename = create_template(input_files['tmpl_pars'], 'pars',params,resultDir+"/input/input_"+str(i),check_params, input_files['def_ls_pars'])
         client.connect('local_fortran_example',username=os.getenv('CELERY_SSH_USER'),password=os.getenv('CELERY_SSH_PASSWORD')) # Jian: 20220930 - use the "local_fortran_example", which will be wroten a Docker named as model_name
         # Jian: change the argments in TECO to parameters, forcing data, resultDir, mode, da_pars, obs_file, 
-        ssh_cmd = "./test {0} {1} {2} {3} {4} {5}".format(param_filename, input_files["forcing"], 'input/SPRUCE_obs.txt', resultDir+'/output_'+str(i)+'/', '0', 'input/SPRUCE_da_pars.txt')
+        ssh_cmd = "./test {0} {1} {2} {3} {4} {5}".format(param_filename, input_files["forcing"], 'input/SPRUCE_obs.txt', resultDir+'/output/ouput_'+str(i)+'/', '0', 'input/SPRUCE_da_pars.txt')
         print(ssh_cmd)
         stdin, stdout, stderr = client.exec_command(ssh_cmd)
         #     stdin, stdout, stderr = client.exec_command(ssh_cmd)
         result = str(stdout.read())
-        # import pandas as pd
-        dates = pd.read_csv(resultDir+'/output/Simu_dailyflux14001.txt')
-        dates.columns = ["sdoy", "GPP", "NEE", "ER", "NPP", "Ra", "QC1", "QC2", "QC3", "QC4", "QC5", "QC6", "QC7", "QC8", "Rh"]
-        result_file_path='/webData/show_forecast_results/'+"lastest_forecast_results_380ppm_0degree.txt" # Jian: the path in TECO docker that links to "/web/data"
-        dates.to_csv(result_file_path, index=None) 
+    # import pandas as pd
+    dates = pd.read_csv(resultDir+'/output/output_1/Simu_dailyflux14001.txt')
+    dates.columns = ["sdoy", "GPP", "NEE", "ER", "NPP", "Ra", "QC1", "QC2", "QC3", "QC4", "QC5", "QC6", "QC7", "QC8", "Rh"]
+    result_file_path='/webData/show_forecast_results/'+"lastest_forecast_results_380ppm_0degree.txt" # Jian: the path in TECO docker that links to "/web/data"
+    dates.to_csv(result_file_path, index=None) 
         # 2. forcing
 
 @app.task(bind=True)
